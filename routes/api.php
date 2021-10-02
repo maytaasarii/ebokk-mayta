abedc4438de449da17d1501ee4bce9d31a96c2fe
maytaasarii <maytaaermayy@gmail.com>
2021-10-02 20:03:14 +0700

    first commit

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use GuzzleHttp\Promise\create;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+    Route::get('/books',[BookController::class, 'index']);
+    Route::post('/books',[BookController::class, 'store']);
+    Route::get('/books/{id}',[BookController::class, 'show']);
+    Route::put('/books/{id}',[BookController::class, 'update']);
+    Route::delete('/books/{id}',[BookController::class, 'destroy']);
+
+Route::resource('Book',BookController::class)->except('create','edit');
